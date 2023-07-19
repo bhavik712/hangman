aea2c144b988285916cf78b7f54f5a6883c3bd45
bhavik712 <baradbhavik41@gmail.com>
2023-07-19 16:51:20 +0530

    this is final refactored code

--- game.js
+++ game.js
@@ -1,74 +1,32 @@
-const Hangman = function(word,remainingGuesses){
-    this.word = word;
-    this.remainingGuesses = remainingGuesses;
-    this.guessedletters = [];
-    this.status = 'Playing';
-}
 
-Hangman.prototype.getPuzzle= function (){
-    let puzzle = '';
-    const wordArray = this.word.split('');
 
-    wordArray.forEach((letter)=>{
-      if(this.guessedletters.includes(letter)|| letter === ' '){
-        puzzle += letter;
-      }
-      else{
-        puzzle += '*';
-      }
-    })
 
-    document.querySelector('#puzzle-word').textContent = puzzle;
-}
-
-Hangman.prototype.guessedLetters = function (letter){
-    if(this.remainingGuesses){
-        if(!this.guessedletters.includes(letter)){
-            this.guessedletters.push(letter);
-            if(!this.word.toLowerCase().includes(letter)){
-                this.remainingGuesses--;
-            }
-            return (`You have ${this.remainingGuesses} chance left`)
-        }
-        else{
-            return ('ohh already guessed character');
-        }
-    }
-    else{
-        return ('Sorry your guess chances are completed.')
-    }
-    
-}
-
-Hangman.prototype.calculateStatus = function(){
-    if(!this.remainingGuesses){
-        this.status = 'Lost';
-    }
-    else{
-        let finished = true;
-        const wordArray = this.word.split('');
-
-        finished = wordArray.every((letter)=> this.guessedletters.includes(letter))
-        if(finished){
-            this.status = 'Won';
-        }
-        else{
-            this.status ='Playing';
-        }
-
-    }
-    return this.status;
-}
 const game1 = new Hangman('chair',2);
 const game2 = new Hangman('New Body',4);
 // onsole.log(game1.getPuzzle());
 
-game1.getPuzzle();
+showPuzzle = () =>{
+    const puzzleWord = game1.getPuzzle();
+    const puzzleEle = document.querySelector('#puzzle-word')
+    puzzleEle.textContent = puzzleWord;
+}
+
+showStatus = () => {
+    game1.calculateStatus();
+    const statusEle = document.querySelector('#show-status')
+    statusEle.textContent = game1.status;
+
+}
+
+showPuzzle();
+
 window.addEventListener('keypress',(e)=>{
     const letter = (String.fromCharCode(e.charCode)).toLocaleLowerCase();
-    const guessRespone = (game1.guessedLetters(letter));
-    document.querySelector('#guess-response').textContent = guessRespone;
-    game1.getPuzzle();
-    document.querySelector('#show-status').textContent = game1.calculateStatus();
+    (game1.guessedLetters(letter));
+    showPuzzle();
+    showStatus();
+    const message = game1.showMessage();
+    const messageEle = document.querySelector('#show-message')
+    messageEle.textContent = message;
 
 })
